00d9b00868b921b0f9f2643022b5e8975710537a
Manish007 <dhanetwalmanish@yahoo.com>
2021-04-21 21:47:35 +0800

    Add files via upload

---
+++ src/datalogger_new.py
@@ -0,0 +1,153 @@
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.patheffects as path_effects
+
+# Data Logger object
+class dataLogger(object):
+        """docstring for dataLogger"""
+        def __init__(self,logType):
+            self.ticktime = []
+            self.ticks = []
+            self.exeTime = []
+            self.x_ref = []
+            self.y_ref = []
+            self.z_ref = []
+            self.x_pos = []
+            self.y_pos = []
+            self.z_pos = []
+            self.phi_ref    = []
+            self.theta_ref  = []
+            self.thrust_ref = []
+            self.u_smc   = []
+            self.u_nn  = []
+            self.nLayer  = []
+            self.nNodes  = []
+            self.timestr = time.strftime("%Y%m%d-%H%M%S")
+
+            if logType == 'flight':
+                self.logFile = open("FlightStateLog"+self.timestr+".txt", "w+")
+            else:
+                self.logFile = open(logType+"-ControlLog"+self.timestr+".txt", "w+")
+                 
+        def appendStateData(self,t,ext,xr,yr,zr,xa,ya,za,phir,thetar,thrustr):
+            self.ticktime.append(t)
+            self.exeTime.append(ext)
+            self.x_ref.append(xr)
+            self.y_ref.append(yr)
+            self.z_ref.append(zr)
+            self.x_pos.append(xa)
+            self.y_pos.append(ya)
+            self.z_pos.append(za)
+            self.phi_ref.append(phir)
+            self.theta_ref.append(thetar)
+            self.thrust_ref.append(thrustr)
+            
+
+        def saveStateData(self,t,ext,xr,yr,zr,xa,ya,za,phir,thetar,thrustr):
+            self.logFile.writelines("%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f\n"%\
+                (t,ext,xr,yr,zr,xa,ya,za,phir,thetar,thrustr))
+
+        def appendControlData(self,t,usmc,unn,layer,node):
+            self.ticks.append(t)
+            self.u_smc.append(usmc)
+            self.u_nn.append(unn)
+            self.nLayer.append(layer)
+            self.nNodes.append(node)
+            
+        def saveControlData(self,t,usmc,unn,layer,node):
+            self.logFile.writelines("%f;%f;%f;%d;%d\n"%\
+                (t,usmc,unn,layer,node))
+
+        def plotFigure(self):
+            print ('Plotting the results..')
+            exr   = np.array(self.x_ref)
+            ex    = np.array(self.x_pos)
+            eyr   = np.array(self.y_ref)
+            ey    = np.array(self.y_pos)
+            ezr   = np.array(self.z_ref)
+            ez    = np.array(self.z_pos)
+            sqrex = np.multiply((exr-ex),(exr-ex))
+            sqrey = np.multiply((eyr-ey),(eyr-ey))
+            sqrez = np.multiply((ezr-ez),(ezr-ez))
+            rmsex = np.sqrt(sum(sqrex)/np.size(ex))
+            rmsey = np.sqrt(sum(sqrey)/np.size(ey))
+            rmsez = np.sqrt(sum(sqrez)/np.size(ez))
+            print ('RMSE XYZ = ',rmsex,rmsey,rmsez)
+
+            fig = plt.figure("XYZ plot",figsize=(16,9))
+            ax1  = plt.subplot2grid((6,6),(0,0),projection='3d', colspan=4,rowspan=6)
+            ax1.plot3D(self.x_ref,self.y_ref,self.z_ref, 'k--',label='Target',color='red')
+            ax1.plot3D(self.x_pos,self.y_pos,self.z_pos, label='Drone trajectory',color='blue')
+            ax1.set_xlabel('x')
+            ax1.set_ylabel('y')
+            ax1.set_zlabel('z')
+            ax1.set_xlim3d(-2,2)
+            ax1.set_ylim3d(-2,2)
+            ax1.set_zlim3d(0,2)
+            ax1.legend()
+
+            ax2 = plt.subplot2grid((6,6),(0,4),colspan=2,rowspan=2)
+            ax2.plot(self.ticktime,self.x_ref,self.ticktime,self.x_pos)
+            ax2.grid(True)
+            ax2.set_xlabel ('time (s)')
+            ax2.set_ylabel('X (m)')
+            #ax2.text(2, 1, 'RMSE X = %.2f'%rmsex, fontsize=10)
+
+            ax3 = plt.subplot2grid((6,6),(2,4),colspan=2,rowspan=2)
+            ax3.plot(self.ticktime,self.y_ref,self.ticktime,self.y_pos)
+            ax3.grid(True)
+            ax3.set_xlabel ('time (s)')
+            ax3.set_ylabel('Y (m)')
+            #ax3.text(2, 1, 'RMSE Y = %.2f'%rmsey, fontsize=10)
+
+            ax4 = plt.subplot2grid((6,6),(4,4),colspan=2,rowspan=2)
+            ax4.plot(self.ticktime,self.z_ref,self.ticktime,self.z_pos)
+            ax4.grid(True)
+            ax4.set_xlabel ("time(s)")
+            ax4.set_ylabel('Z (m)')
+            ax4.text(1, 2, 'RMSE Z = %.2f'%rmsez, fontsize=10)
+
+            plt.show()
+
+
+        def plotControlData(self):
+            print ('Plotting the control signal..')
+            plt.figure("Control signal")
+            plt.plot(self.ticks,self.u_smc,label='SMC')
+            plt.plot(self.ticks,self.u_nn,label='NN')
+            plt.grid(True)
+            plt.xlabel ("time(s)")
+            plt.ylabel('control signal')
+            plt.legend()
+
+            plt.figure("evolution")
+            plt.plot(self.ticks,self.nLayer,label='layer')
+            plt.plot(self.ticks,self.nNodes,label='node')
+            plt.grid(True)
+            plt.xlabel ("time(s)")
+            plt.ylabel('nodes/layers')
+            plt.legend()
+
+            plt.show()
+
+def saveParameters(runType,netX,netY,netZ):
+
+    logW = open(runType+"_Weights"+time.strftime("%Y%m%d-%H%M%S")+".txt", "w+")
+    
+    logW.writelines("X-net: "+str(netX)+"\n")
+    for i in range(len(netX)):
+        logW.writelines("weight of layer-%d = "%i + str(netX[i].linear.weight.data)+"\n")
+    
+    logW.writelines("\n")
+    logW.writelines("Y-net: "+str(netY)+"\n")
+    for i in range(len(netY)):
+        logW.writelines("weight of layer-%d = "%i + str(netY[i].linear.weight.data)+"\n")
+    
+    logW.writelines("\n")
+    logW.writelines("Z-net: "+str(netZ)+"\n")
+    for i in range(len(netZ)):
+        logW.writelines("weight of layer-%d = "%i + str(netZ[i].linear.weight.data)+"\n")
+
+    logW.close()
---
+++ src/run_iris_PID.py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python
+
+# ROS python API
+import rospy
+
+# 3D point & Stamped Pose msgs
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Quaternion, Vector3
+
+# import all mavros messages and services
+from mavros_msgs.msg import *
+from mavros_msgs.srv import *
+
+#import pymavlink
+from mavros_msgs.msg import AttitudeTarget
+#from std_msgs.msg import Header
+from tf.transformations import quaternion_from_euler
+from tf.transformations import euler_from_quaternion
+
+import time
+import math
+import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib.patheffects as path_effects
+from termcolor import colored, cprint
+import copy
+import pdb
+
+# import torch
+# import torch.nn as nn
+# from torch.autograd import Variable
+# import torch.nn.functional as F
+# import torch.utils.data as Data
+
+from evonn import *
+from px4api import *
+from datalogger import *
+from tools import *
+
+
+# Main function
+def main():
+# INITIAL SETUP
+    # INITIALIZE ADAPTIVE NETWORK PARAMETERS:
+    N_INPUT       = 3                   # Number of input network
+    N_OUTPUT      = 1                   # Number of output network
+    INIT_NODE     = 3                   # Number of node(s) for initial structure
+    INIT_LAYER    = 2                   # Number of initial layer (minimum 2, for 1 hidden and 1 output)
+    N_WINDOW      = 500                 # Sliding Window Size
+    EVAL_WINDOW   = 5                   # Evaluation Window for layer Adaptation
+    DELTA         = 0.05                # Confidence Level for layer Adaptation (0.05 = 95%)
+    ETA           = 0.0001              # Minimum allowable value if divided by zero
+    FORGET_FACTOR = 0.98                # Forgetting Factor of Recursive Calculation
+    #EXP_DECAY     = 1                   # Learning Rate decay factor
+    #LEARNING_RATE = 0.00005             # Network optimization learning rate
+    LEARNING_RATE = 0.0003            	# Network optimization learning rate
+    WEIGHT_DECAY  = 0.000125            # Regularization weight decay factor
+    #WEIGHT_DECAY  = 0.0                 # Regularization weight decay factor
+
+    # INITIALIZE SYSTEM AND SIMULATION PARAMETERS:
+    IRIS_THRUST   = 0.5629                  # Nominal thrust for IRIS Quadrotor
+    RATE          = 30.0                    # Sampling Frequency (Hz)
+    # PID_GAIN_X    = [0.25,0.0,0.02]         # PID Gain Parameter [KP,KI,KD] axis-X
+    # PID_GAIN_Y    = [0.25,0.0,0.02]         # PID Gain Parameter [KP,KI,KD] axis-Y
+    PID_GAIN_X    = [0.15,0.0,0.004]         # PID Gain Parameter [KP,KI,KD] axis-X
+    PID_GAIN_Y    = [0.15,0.0,0.004]         # PID Gain Parameter [KP,KI,KD] axis-Y
+    PID_GAIN_Z    = [0.013,0.0005,0.2]      # PID Gain Parameter [KP,KI,KD] axis-Z
+    #PID_GAIN_Z    = [0.013,0.0,0.2]         # PID Gain Parameter [KP,KI,KD] axis-Z
+    SIM_TIME      = 250                     # Simulation time duration (s)
+
+    # Initial conditions of UAV system
+    xref     = 0.0
+    yref     = 0.0
+    zref     = 2.0
+    interrx  = 0.0
+    interry  = 0.0
+    interrz  = 0.0
+    errlastx = 0.0
+    errlasty = 0.0
+    errlastz = 0.0
+    runtime  = 0.0
+
+# CONTROLLER WARM-UP
+    # Ignite the Evolving NN Controller
+    Xcon = NetEvo(N_INPUT,N_OUTPUT,INIT_NODE)
+    Ycon = NetEvo(N_INPUT,N_OUTPUT,INIT_NODE)
+    Zcon = NetEvo(N_INPUT,N_OUTPUT,INIT_NODE)
+
+    if INIT_LAYER > 2:
+        #pdb.set_trace()
+        for i in range(INIT_LAYER-2):
+            Xcon.addhiddenlayer()
+            Ycon.addhiddenlayer()
+            Zcon.addhiddenlayer()
+
+    dt      = 1/RATE
+    Xcon.dt = dt
+    Ycon.dt = dt
+    Zcon.dt = dt
+    Xcon.smcpar = PID_GAIN_X
+    Ycon.smcpar = PID_GAIN_Y
+    Zcon.smcpar = PID_GAIN_Z
+
+    # PX4 API object
+    modes = fcuModes()              # Flight mode object
+    uav   = uavCommand()            # UAV command object
+
+    # Data Logger object
+    logData = dataLogger()
+    xconLog = dataLogger()
+    yconLog = dataLogger()
+    zconLog = dataLogger()
+
+    # Initiate node and subscriber
+    rospy.init_node('setpoint_node', anonymous=True)
+    rate  = rospy.Rate(RATE)        # ROS loop rate, [Hz]
+
+    # Subscribe to drone state
+    rospy.Subscriber('mavros/state', State, uav.stateCb)
+
+    # Subscribe to drone's local position
+    rospy.Subscriber('mavros/local_position/pose', PoseStamped, uav.posCb)
+
+    # Setpoint publisher
+    att_sp_pub = rospy.Publisher('mavros/setpoint_raw/attitude', AttitudeTarget, queue_size=10)
+
+    # Initiate Attitude messages
+    att             = AttitudeTarget()
+    att.body_rate   = Vector3()
+    att.orientation = Quaternion(*quaternion_from_euler(0.0, 0.0,0.0))
+    att.thrust      = IRIS_THRUST
+    att.type_mask   = 7 
+
+# TAKEOFF and MISSION WARM-UP
+    # Arming the UAV --> no auto arming
+    text = colored("Ready for Arming..Press Enter to continue...", 'green', attrs=['reverse', 'blink'])
+    raw_input(text)
+    while not uav.state.armed:
+        modes.setArm()
+        rate.sleep()
+        text = colored('Vehicle is arming..', 'yellow')
+        print text
+    
+    # Switch to OFFBOARD after send few setpoint messages
+    text = colored("Vehicle Armed!! Press Enter to switch OFFBOARD...", 'green', attrs=['reverse', 'blink'])
+    raw_input(text)
+
+    while not uav.state.armed:
+        modes.setArm()
+        rate.sleep()
+        text = colored('Vehicle is arming..', 'yellow')
+        print text
+
+    rate.sleep()
+    k=0
+    while k<10:
+        att_sp_pub.publish(att)
+        rate.sleep()
+        k = k+1
+    modes.setOffboardMode()
+    text = colored('Now in OFFBOARD Mode..', 'blue', attrs=['reverse', 'blink'])
+    print text
+    
+# ROS Main loop::
+    k = 0
+    while not rospy.is_shutdown():
+        start = time.time()
+        rate.sleep()
+
+        # Setpoint generator
+        if runtime <= 20:
+            xref = 0.0
+            yref = 0.0
+            zref = runtime* 1.5/20.0
+        elif runtime > 20 and runtime <=30:
+            xref = (runtime-20)* 1.0/10.0
+            #xref = (runtime-20)* 5.0/10.0
+            yref = 0.0
+            zref = 1.5
+        elif runtime>30 and runtime <=200:
+            xref = 0.0+1.0*math.cos(math.pi*(runtime-30)/20.0)
+            yref = 0.0+1.0*math.sin(math.pi*(runtime-30)/20.0)
+            #xref = 0.0+5.0*math.cos(math.pi*(runtime-30)/60.0)
+            #yref = 0.0+5.0*math.sin(math.pi*(runtime-30)/60.0)
+            zref = 1.5
+            if runtime == 200:
+                xs = uav.local_pos.x
+                ys = uav.local_pos.y
+                zs = uav.local_pos.z
+                inc = 0.0
+        else:
+            inc += 0.005
+            xref = xs+inc
+            yref = ys+inc
+            zref = zs*np.exp(-1/12.0*(runtime-200))
+            
+            if zref < 0.0:
+                zref = 0.0
+
+        # update current position
+        xpos = uav.local_pos.x
+        ypos = uav.local_pos.y
+        zpos = uav.local_pos.z
+        
+        # calculate errors,delta errors, and integral errors
+        errx,derrx,interrx = Xcon.calculateError(xref,xpos)
+        erry,derry,interry = Ycon.calculateError(yref,ypos)
+        errz,derrz,interrz = Zcon.calculateError(zref,zpos)
+
+        # PID Controller equations
+        #theta_ref = 0.04*errx+0.0005*interrx+0.01*derrx
+        #phi_ref   = 0.04*erry+0.0005*interry+0.01*derry
+        vx = PID_GAIN_X[0]*errx+PID_GAIN_X[1]*interrx+PID_GAIN_X[2]*derrx   # Velocity X
+        vy = PID_GAIN_Y[0]*erry+PID_GAIN_Y[1]*interry+PID_GAIN_Y[2]*derry   # Velocity Y
+        thrust_ref = IRIS_THRUST+PID_GAIN_Z[0]*errz+PID_GAIN_Z[1]*interrz+PID_GAIN_Z[2]*derrz	# Additional Thrust Z
+       
+        euler = euler_from_quaternion(uav.q)
+        psi   = euler[2]
+        phi_ref,theta_ref = transform_control_signal(vx,vy,psi)
+
+        # control signal limiter
+        #phi_ref    = limiter(-1*phi_ref,0.2)
+        phi_ref    = limiter(-1*phi_ref,0.25)
+        theta_ref  = limiter(-1*theta_ref,0.25)
+        att.thrust = limiter(thrust_ref,1)
+
+        # Phi, Theta and Psi quaternion transformation
+        att.orientation = Quaternion(*quaternion_from_euler(phi_ref, theta_ref, 0.0))
+
+        # Publish the control signal
+        att_sp_pub.publish(att)
+        
+        # Print all states
+        print 'Xr,Yr,Zr    = ',xref,yref,zref
+        print 'X, Y, Z     = ',uav.local_pos.x,uav.local_pos.y,uav.local_pos.z
+        print 'phi, theta  = ',phi_ref,theta_ref
+        print 'att angles  = ',euler
+        print 'Thrust      = ',att.thrust
+        
+        k+=1
+        runtime = k*dt
+
+        # Append Data Logger
+        logData.appendStateData(runtime,xref,yref,zref,xpos,ypos,zpos,phi_ref,theta_ref,thrust_ref)
+        xconLog.appendControlData(runtime,vx,Xcon.un,Xcon.u,Xcon.nNodes)
+        yconLog.appendControlData(runtime,vy,Ycon.un,Ycon.u,Ycon.nNodes)
+        zconLog.appendControlData(runtime,thrust_ref,Zcon.un,Zcon.u,Zcon.nNodes)
+
+        print 'Runtime    = ',runtime
+        print 'Exec time  = ', time.time()-start
+        
+        # Break condition
+        if runtime > SIM_TIME:
+            modes.setRTLMode()
+
+            text = colored('Auto landing mode now..', 'blue', attrs=['reverse', 'blink'])
+            print text 
+            
+            logData.plotFigure()
+            xconLog.plotControlData()
+            yconLog.plotControlData()
+            zconLog.plotControlData()
+            logData.saveAll()
+
+            text = colored('Mission Complete!!', 'green', attrs=['reverse', 'blink'])
+            print text
+            break
+
+# Call main function
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
